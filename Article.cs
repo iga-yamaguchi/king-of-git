6c55d5cdfc0e8ff8422a136d0ea2659220fd3637
iga-yamaguchi <iganin2@gmail.com>
2017-12-13 15:38:54 +0900

    Add Article

---
+++ Article.cs
@@ -0,0 +1,11 @@
+﻿namespace SampleMvc.Models
+{
+    public class Article
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Content { get; set; }
+    }
+}
